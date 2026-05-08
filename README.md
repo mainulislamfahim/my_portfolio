99a880f3444c3058f12b18edeefc6faa6ece614a
Mainul <mainul.ifa@gmail.com>
2026-05-09 02:41:27 +0600

    update UI

--- README.md
+++
@@ -1,1 +0,0 @@
-# Mainul-Portfolio
--- lib/globals/contents.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Contents {
-  static SizedBox sizedBox({height, width}) {
-    return sizedBox(height: height, width: width);
-  }
-}
